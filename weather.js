8ec7edbf5fc85898531722f71a1ad552c405306a
Rudra-Pratap-Singh1331 <rudrapsingh121@gmail.com>
2024-11-24 19:26:13 +0530

    weather.js

--- weather.js
+++ weather.js
@@ -57,67 +57,87 @@ let FetchDetails=async (data)=>{
         </div>
       </div>`
    searchField.value='';
-  if (FinalResult.main.temp < 0) {
+  if (FinalResult.weather.main === 'Clouds') {
         if(date.getHours()>=18){
-          bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/1601039994/photo/bow-shaped-milky-way-above-hanles-peak.webp?a=1&b=1&s=612x612&w=0&k=20&c=Jd9lwlkSVmrYpaFy5xdx7oeq5NlPXbVuq5IbMRWDuVA=">`
+          bgimg.innerHTML=`<img class="image" src="https://img.freepik.com/premium-photo/full-moon-cloudy-night-sky-aesthetic-background_198067-173620.jpg">`  //clody night image
           bgimg.style.filter='blur(3px)';
           darktheme();
          
         }else if(date.getHours()>=6 && date.getHours()<=12){
-          bgimg.innerHTML=`<img class="image" src="https://images.unsplash.com/photo-1703510092630-6d6a89cfa6e5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">`
+          bgimg.innerHTML=`<img class="image" src="https://images.unsplash.com/photo-1519541312928-0872bb28207b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">`
           bgimg.style.filter='blur(3px)'
           lightmode();
-  }
-    else{
-      bgimg.innerHTML=`<img src="https://images.unsplash.com/photo-1703510092630-6d6a89cfa6e5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">`
-      bgimg.style.filter='blur(3px)'
-      lightmode();
-    }
+          }
+        else{
+          bgimg.innerHTML=`<img src="https://media.istockphoto.com/id/1300986545/photo/aerial-cinematic-shot-of-the-long-sprawling-suburbs-of-ljubljana-on-a-cloudy-day.jpg?s=612x612&w=0&k=20&c=b2PnJH_4zth91WnkgmEUQ-1-HfbuxzLVQL_STp0dC4k=">`
+          bgimg.style.filter='blur(3px)'
+          lightmode();
+          }
      // Freezing
-  } else if (FinalResult.main.temp >= 0 && FinalResult.main.temp <= 15) {
+  } else if (FinalResult.weather.main === 'Haze') {
     if(date.getHours()>=18){
-      bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/1601039994/photo/bow-shaped-milky-way-above-hanles-peak.webp?a=1&b=1&s=612x612&w=0&k=20&c=Jd9lwlkSVmrYpaFy5xdx7oeq5NlPXbVuq5IbMRWDuVA=">`
+      bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/614122930/photo/spooky-foggy-forest.jpg?s=612x612&w=0&k=20&c=jBKT8Ku8a5VGMclYsX3LttZUpsbVdfO-XSeYQEQIqUs=">`
       bgimg.style.filter='blur(3px)'
       darktheme();
     }
+      else if(date.getHours()>=6 && date.getHours()<=12){
+          bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/1089026982/photo/image-of-winter-fog-scene-in-delhi-with-india-gate-as-a-background.jpg?s=612x612&w=0&k=20&c=rXSudGCCelYGe1O4y8Ix3txjVC-UdBlCLa8DIgRr4mg=">`
+          bgimg.style.filter='blur(3px)'
+          lightmode();
+          }
      else{
-    bgimg.innerHTML=`<img class="image" src="https://images.unsplash.com/photo-1487621167305-5d248087c724?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">`
+    bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/2181297300/photo/close-up-of-a-motorbike-on-field-against-trees.jpg?s=612x612&w=0&k=20&c=4BxWxIJDBiXec_pDoV_rsw2MRs5aV1e2I0ZxKgu0Lr4=">`
     bgimg.style.filter='blur(7px)'
     lightmode();
   }
   
   } 
-  else if (FinalResult.main.temp > 15 && FinalResult.main.temp <= 25) {
+  else if (FinalResult.weather.main === 'Clear') {
     if(date.getHours()>=18){
+       bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/162515751/photo/moon-over-mountains.jpg?s=612x612&w=0&k=20&c=mKsWiAqN8lOGl6bdnDg9ssY25RSJJbSOk3g0MuDZNXw=">`
+     bgimg.style.filter='blur(3px)'
      darktheme();
-    }else{
-    bgimg.innerHTML=`<img class="image" src="https://images.unsplash.com/photo-1583054968714-b8d4317377b4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWlsZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D">`
+    }
+       else if(date.getHours()>=6 && date.getHours()<=12){
+          bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/1324413691/photo/beautiful-sky-with-white-clouds.jpg?s=612x612&w=0&k=20&c=40a8JIoRJ1BAI4XeyEouZl0QbVAJToDBRQn31S3FGzk=">`
+          bgimg.style.filter='blur(3px)'
+          lightmode();
+          }
+    else{
+    bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/463594503/photo/mountain-landscape-the-himalayas.jpg?s=612x612&w=0&k=20&c=Kg2TdVazkrzbOSk-7OZ9ueweEVXJnZiSfADeWpfxxW8=">`
     bgimg.style.filter='blur(7px)'
    lightmode();
   }
   }
-   else if (FinalResult.main.temp > 25 && FinalResult.main.temp <= 35) {
+   else if (FinalResult.weather.main === 'Rain') {
     if(date.getHours()>=18){
-      bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/1601039994/photo/bow-shaped-milky-way-above-hanles-peak.webp?a=1&b=1&s=612x612&w=0&k=20&c=Jd9lwlkSVmrYpaFy5xdx7oeq5NlPXbVuq5IbMRWDuVA=">`
+      bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/831057314/photo/rain-night.jpg?s=612x612&w=0&k=20&c=Ir3t1WiPDHmbOAY3cDqduhjLW-JUAZg2F1Izn_r0l5M=">`
       bgimg.style.filter='blur(3px)'
       darktheme();
      
-    }else{
-    bgimg.innerHTML=`<img class="image" src="https://images.unsplash.com/photo-1565677913671-ce5a5c0ae655?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">`
+    }else if(date.getHours()>=6 && date.getHours()<=12){
+          bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/819632426/photo/raindrops-on-glass-window.jpg?s=612x612&w=0&k=20&c=pdVUIcdeUxOKCV5B7WOS8oYzxyHL9ADuYnxnt0_0glE=">`
+          bgimg.style.filter='blur(3px)'
+          lightmode();
+          }
+    else{
+    bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/453684353/photo/rain-at-the-fields.jpg?s=612x612&w=0&k=20&c=JXVnwl83Oifw3ook_yhZy9IIeHm2Ey6PrxgZUK1_vZs=">`
     bgimg.style.filter='blur(7px)'
     lightmode();
   }
   
   }
-   else if (FinalResult.main.temp > 35) {
+   else if (FinalResult.weather.main === 'Thunderstorm') {
     if(date.getHours()>=18){
-      bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/1601039994/photo/bow-shaped-milky-way-above-hanles-peak.webp?a=1&b=1&s=612x612&w=0&k=20&c=Jd9lwlkSVmrYpaFy5xdx7oeq5NlPXbVuq5IbMRWDuVA=">`
+      bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/1318748572/photo/massive-lightning-strike-over-the-brisbane-city-suburbs-lights.jpg?s=612x612&w=0&k=20&c=9Z5tynrQYH3E0fruCBlwIbgsgbdu5_DHxLbSu44o3co=">`
       bgimg.style.filter='blur(3px)'
      darktheme();
      
-    }else{
-   lightmode();
-    }
+    }else {
+          bgimg.innerHTML=`<img class="image" src="https://media.istockphoto.com/id/1653255150/photo/forked-lightning-strikes-down-behind-a-hill.jpg?s=612x612&w=0&k=20&c=Hb98pd047Q7KOr6dwzHJfIwIIajEe0eivDEUMpe1dD4=">`
+          bgimg.style.filter='blur(3px)'
+          lightmode();
+          }
   }
 
   }catch(error){
